d34593f05c7b01ba84dabacec91b12cde4139ce4
karen-velasquez <maritza.karen.flores.velasquez@gmail.com>
2022-08-18 09:57:10 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,17 @@
-
 import streamlit as st
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 st.write("HELLO MOJIX")
+file =  st.file_uploader('upload a csv', type='csv')
+if file:
+    df = pd.read_csv(file)
+    st.dataframe(df)
+    st.markdown("---")
+    fig1 = plt.figure(figsize=(10,4))
+    sns.countplot(x='Pclass',data=df)
+    
+    st.pyplot(fig1)
+    fig2 = plt.figure(figsize=(10,4))
